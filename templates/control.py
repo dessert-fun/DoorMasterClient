60caf4ac39b18f7e3cdb78fa4efda8570337f839
MarkXu <xubiaolin@markxu.vip>
2019-09-02 22:56:51 +0800

    完成信息显示的Ui修改,层叠样式需要修改

---
+++ templates/control.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'control.html'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
